ce616adf8af90b87d079d8b56d0ee0373b0566dd
Paul LE GUILLOU <paul.leguillou@bforbank.com>
2024-07-01 09:14:30 +0200

    feat: rename mongodb collection

--- src/main/java/com/bforbank/testapp/view/entity/CustomerWithAccounts.java
+++ src/main/java/com/bforbank/testapp/view/entity/CustomerWithAccounts.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collection = "accounts")
+@Document(collection = "customers")
 public class CustomerWithAccounts {
 
     @Id
